57b9ab2b5c038b580060c4cb3c99b1c9d27c0c18
Carlos Rodriguez <carlos@eddorre.com>
2010-04-17 11:28:40 -0700

    Using jRails from rubygems instead of Github

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "faker"
-  config.gem "aaronchi-jrails", :lib => false, :version => "0.5.1"
+  config.gem "jrails", :lib => false
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
